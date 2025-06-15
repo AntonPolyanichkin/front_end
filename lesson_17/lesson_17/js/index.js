03629455fcc2f49ecbb46edf49496e23485ce1c0
«Anton» <antonpolyanichkin@gmail.com>
2025-06-16 00:29:16 +0300

    Was done

--- lesson_17/lesson_17/js/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("click", menuAction)
-
-function menuAction(e) {
-  const target = e.target
-  if(target.closest(".button-burger")) {
-    document.body.classList.toggle("menu-active")
-  }
-}
