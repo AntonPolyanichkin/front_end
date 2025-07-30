4cc3ad49b364b25758666b9d4514c33a3f6474f0
«Anton» <antonpolyanichkin@gmail.com>
2025-07-30 17:29:23 +0300

    Was done

--- hydra/hydra/js/index.js
+++ hydra/hydra/js/index.js
@@ -51,24 +51,41 @@ document.body.addEventListener("click", (e) => {
   }
 });
 
-// const swiper = document.querySelector(".swiper");
-// let createSwiper
-// function showSwiper(media) {
-//   if (media.matches) {
-//     swiper.classList.add("swiper-active");
-// 	 createSwiper = new Swiper(".swiper", {
-// 		slidesPerView: 1,
-// 		speed: 400,
-// 		spaceBetween: 20,
-// 		navigation: {
-// 			nextEl: '.swiper-button-next',
-// 			prevEl: '.swiper-button-prev',
-// 		 }
-// 	 });
-//   }
-//   else{
-// 	swiper.classList.remove("swiper-active");
-//   }
-// }
-// dynamicAdap(showSwiper, swiperAppear);
+const sliderWrapper = document.querySelector(".contacts-information__slider-wrapper");
+const sliderElement = document.querySelectorAll(".element-information");
+const nextSliderBtn = document.querySelector(".contacts-information__next-btn");
+const prevSliderBtn = document.querySelector(".contacts-information__prev-btn");
 
+function getMaxOffset() {
+  return sliderWrapper.scrollWidth - sliderWrapper.getBoundingClientRect().width;
+}
+
+let shift = 0;
+function getSlideWidth(sliderElement) {
+  return sliderElement[0].offsetWidth;
+}
+
+nextSliderBtn.addEventListener("click", () => {
+  const slideWidth = getSlideWidth(sliderElement);
+  const maxOffset = getMaxOffset();
+
+  if (shift + slideWidth <= maxOffset) {
+    shift += slideWidth;
+    sliderWrapper.style.transform = `translateX(-${shift}px)`;
+  }
+});
+prevSliderBtn.addEventListener("click", () => {
+  const slideWidth = getSlideWidth(sliderElement);
+  const maxOffset = getMaxOffset();
+
+  if (shift - slideWidth >= 0) {
+    shift -= slideWidth;
+    sliderWrapper.style.transform = `translateX(-${shift}px)`;
+  }
+});
+
+window.addEventListener("resize", () => {
+  shift = 0;
+  getSlideWidth(sliderElement);
+  getMaxOffset();
+});
